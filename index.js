f7c7d40f8c24ecc51bb1b52053174c0b72d86cd6
Veronica Bustabad <vbustabad@gmail.com>
2018-09-11 03:04:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Your code here
 
-const justInvoke(returnsThisAndArgs) = {
+function justInvoke(returnsThisAndArgs) = {
     function() {
       function(returnsThisAndArgs) {
           return this;

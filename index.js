65cfc166d879da54d49bbbd60d4c3e650d9de2a6
Veronica Bustabad <vbustabad@gmail.com>
2018-09-11 03:33:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function setThisWithCall(fn, thisValue, arg) {
 function setThisWithApply(fn, thisValue, args) {
   return fn.apply(thisValue, args);
 }
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue);
+}

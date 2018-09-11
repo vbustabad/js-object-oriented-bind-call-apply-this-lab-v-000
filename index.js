c926e844f75842d69758cef9c646b50a2e4e6d17
Veronica Bustabad <vbustabad@gmail.com>
2018-09-11 03:18:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function justInvoke(fn) {
 function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg);
 }
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, arg);
+}
